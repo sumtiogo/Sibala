4014db68d7fc75156bf4e829290fe34daa2cdd6e
Ayson Chang <ayson.chang@evolutivelabs.com>
2022-01-05 23:15:14 +0800

    refactor to logic: winnerOutput for normal point

--- src/Game.ts
+++ src/Game.ts
@@ -1,4 +1,5 @@
 import { Parser } from "./Parser";
+import { Category } from "./Player";
 
 export class Game {
   showResult(input: string): string {
@@ -6,9 +7,9 @@ export class Game {
     const player1Category = players[0].category;
     const player2Category = players[1].category;
 
+    let winnerName: string;
+    let winnerOutput: string;
     if (player1Category != player2Category) {
-      let winnerName: string;
-      let winnerOutput: string;
       if (player2Category > player1Category) {
         winnerName = players[1].name;
         winnerOutput = players[1].normalPoints.toString();
@@ -17,6 +18,14 @@ export class Game {
         winnerOutput = players[0].normalPoints.toString();
       }
       return `${winnerName} win. - with normal point: ${winnerOutput}`;
+    } else if (player1Category === Category.NormalPoint) {
+      let compareResult = players[0].normalPoints - players[1].normalPoints;
+      if (compareResult != 0) {
+        const winnerPointDices =
+          compareResult > 0 ? players[0].pointDices : players[1].pointDices;
+        winnerName = compareResult > 0 ? players[0].name : players[1].name;
+        return `${winnerName} win. - with normal point: ${winnerPointDices[0]} over ${winnerPointDices[1]}`;
+      }
     }
     return "Tie.";
   }
--- src/Player.ts
+++ src/Player.ts
@@ -2,6 +2,7 @@ export class Player {
   private countMap = {} as Record<string, number>;
   public readonly category: Category;
   public readonly normalPoints: number;
+  public readonly pointDices: number[];
   constructor(public name: string, public dices: string[]) {
     this.dices.forEach((d) => {
       this.countMap[d] = d in this.countMap ? this.countMap[d] + 1 : 1;
@@ -15,6 +16,12 @@ export class Player {
       .filter(([_, c]) => c != 2)
       .map(([k]) => Number(k))
       .reduce((a, b) => a + b);
+
+    this.pointDices = Object.entries(this.countMap)
+      .filter(([_, c]) => c != 2)
+      .map(([k, _]) => Number(k))
+      .sort()
+      .reverse();
   }
 }
 
