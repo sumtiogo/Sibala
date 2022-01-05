df0a5d3d8db260d263430bfcb7764b041cda72e8
Ayson Chang <ayson.chang@evolutivelabs.com>
2022-01-05 18:22:23 +0800

    refactor: extract method

--- src/Game.ts
+++ src/Game.ts
@@ -5,25 +5,23 @@ export class Game {
     // "Black: 3 4 5 6  White: 4 1 4 2",
     // "White win. - with normal point: 3"
     const players = new Parser().parse(input);
-    const countMap1 = {} as Record<string, number>;
-    players[0].dices.forEach((d) => {
-      countMap1[d] = d in countMap1 ? countMap1[d] + 1 : 1;
-    });
-    const player1Category = Object.values(countMap1).some((v) => v === 2)
-      ? Category.NormalPoint
-      : Category.NoPoint;
-    const countMap2 = {} as Record<string, number>;
-    players[1].dices.forEach((d) => {
-      countMap2[d] = d in countMap2 ? countMap2[d] + 1 : 1;
-    });
-    const player2Category = Object.values(countMap2).some((v) => v === 2)
-      ? Category.NormalPoint
-      : Category.NoPoint;
+    const player1Category = this.getCategory(players[0].dices);
+    const player2Category = this.getCategory(players[1].dices);
 
     if (player2Category > player1Category) {
       return "White win. - with normal point: 3";
     }
     return "Tie.";
+  }
+
+  private getCategory(dices: string[]) {
+    const countMap1 = {} as Record<string, number>;
+    dices.forEach((d) => {
+      countMap1[d] = d in countMap1 ? countMap1[d] + 1 : 1;
+    });
+    return Object.values(countMap1).some((v) => v === 2)
+      ? Category.NormalPoint
+      : Category.NoPoint;
   }
 }
 
