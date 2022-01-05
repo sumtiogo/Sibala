80df24e1f5876119d1fe9687141c36aae293b6e3
Ayson Chang <ayson.chang@evolutivelabs.com>
2022-01-05 21:41:38 +0800

    refactor, move getCategory to Player

--- src/Game.ts
+++ src/Game.ts
@@ -5,27 +5,12 @@ export class Game {
     // "Black: 3 4 5 6  White: 4 1 4 2",
     // "White win. - with normal point: 3"
     const players = new Parser().parse(input);
-    const player1Category = this.getCategory(players[0].dices);
-    const player2Category = this.getCategory(players[1].dices);
+    const player1Category = players[0].getCategory();
+    const player2Category = players[1].getCategory();
 
     if (player2Category > player1Category) {
       return `${players[1].name} win. - with normal point: 3`;
     }
     return "Tie.";
   }
-
-  private getCategory(dices: string[]) {
-    const countMap1 = {} as Record<string, number>;
-    dices.forEach((d) => {
-      countMap1[d] = d in countMap1 ? countMap1[d] + 1 : 1;
-    });
-    return Object.values(countMap1).some((v) => v === 2)
-      ? Category.NormalPoint
-      : Category.NoPoint;
-  }
-}
-
-enum Category {
-  NoPoint = 0,
-  NormalPoint = 1,
 }
--- src/Player.ts
+++ src/Player.ts
@@ -1,3 +1,17 @@
 export class Player {
   constructor(public name: string, public dices: string[]) {}
+  public getCategory() {
+    const countMap1 = {} as Record<string, number>;
+    this.dices.forEach((d) => {
+      countMap1[d] = d in countMap1 ? countMap1[d] + 1 : 1;
+    });
+    return Object.values(countMap1).some((v) => v === 2)
+      ? Category.NormalPoint
+      : Category.NoPoint;
+  }
+}
+
+export enum Category {
+  NoPoint = 0,
+  NormalPoint = 1,
 }
