4897076771df7399478e26b42028a36089ed40bf
Ayson Chang <ayson.chang@evolutivelabs.com>
2022-01-05 23:45:56 +0800

    add failed test

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -29,10 +29,10 @@ describe("Game", () => {
       "Black: 5 3 5 4  White: 2 6 2 3",
       "White win. - with normal point: 6 over 3"
     );
-    //
-    // // same sum, bigger point
-    // - Black: 4 1 4 6  White: 3 4 5 5
-    // - Black win. - with normal point: 6 over 1
+    resultShouldBe(
+      "Black: 4 1 4 6  White: 3 4 5 5",
+      "Black win. - with normal point: 6 over 1"
+    );
     //
     // // tie
     // - Black: 3 6 5 5  White: 4 4 3 6
