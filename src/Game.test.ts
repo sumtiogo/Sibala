cf1267d2bad04d237ae986c9b8a46add59042c27
Ayson Chang <ayson.chang@evolutivelabs.com>
2022-01-05 14:10:42 +0800

    group test with describe

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,6 +1,8 @@
 import { Game } from "./Game";
 
-test("no point tie no point", () => {
-  expect(Game.showResult("Black: 1 2 3 4  White: 2 3 4 5")).toEqual("Tie.");
-  expect(Game.showResult("Black: 2 2 2 4  White: 3 3 3 5")).toEqual("Tie.");
+describe("Game", () => {
+  test("no point tie no point", () => {
+    expect(Game.showResult("Black: 1 2 3 4  White: 2 3 4 5")).toEqual("Tie.");
+    expect(Game.showResult("Black: 2 2 2 4  White: 3 3 3 5")).toEqual("Tie.");
+  });
 });
